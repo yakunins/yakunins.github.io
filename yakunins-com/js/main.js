d9b7ecae4bf1270d86ba7957c69882c978874809
Sergey Yakunin <sergey@yakunins.com>
2025-03-25 21:02:50 -0700

    cleanup

--- yakunins-com/js/main.js
+++ yakunins-com/js/main.js
@@ -1,18 +1,32 @@
-// yakunins.com javascript
-
 let delta = 10; // in pixels, gallery's background to be moved if mouse travelled this amount
 jQuery(document).ready(function ($) {
     $("a i").closest("a").addClass("thin-underline");
-    $(".gallery-link span")
-        .addClass("thin-underline")
-        .closest("a")
-        .removeClass("thin-underline");
+    $(".gallery-link span").addClass("thin-underline").closest("a").removeClass("thin-underline");
 
     $.easing.smoothmove = function (x, t, b, c, d) {
         return -c * (t /= d) * (t - 2) + b;
     };
 });
 
+window.addEventListener("load", () => injectGtag("G-KRJWXG2HZQ"));
+const injectGtag = (gid) => {
+    const s1 = document.createElement("script");
+    s1.setAttribute("async", true);
+    s1.setAttribute("src", `https://www.googletagmanager.com/gtag/js?id=${gid}`);
+
+    const s2 = document.createElement("script");
+    s2.textContent = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag("js", new Date());
+        gtag("config", "${gid}");
+    `;
+
+    document.body.append(s1);
+    document.body.append(s2);
+};
+
+/*
 let gamma = "gamma-light";
 jQuery(document).ready(function () {
     setLight();
@@ -32,3 +46,4 @@ function setLight() {
     gamma = "gamma-light";
     $("body").removeClass("gamma-dark").addClass("gamma-light");
 }
+*/
