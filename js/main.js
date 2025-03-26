399808d188fb912cacd29b5d9c52fc18a121e7c6
Sergey Yakunin <sergey@yakunins.com>
2025-03-25 21:03:04 -0700

    ga

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
-// window.addEventListener("load", () => handleLinks());
+window.addEventListener("load", () => injectGtag("G-KRJWXG2HZQ"));
+
+const injectGtag = (gid) => {
+    const s1 = document.createElement("script");
+    s1.setAttribute("async", true);
+    s1.setAttribute(
+        "src",
+        `https://www.googletagmanager.com/gtag/js?id=${gid}`
+    );
+
+    const s2 = document.createElement("script");
+    s2.textContent = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag("js", new Date());
+        gtag("config", "${gid}");
+    `;
+
+    document.body.append(s1);
+    document.body.append(s2);
+};
+
+/*
+window.addEventListener("load", () => handleLinks());
 
 const handleLinks = () =>
     getLinks().forEach((a) => {
@@ -10,3 +33,4 @@ const underlineWhitespaces = (el) =>
 
 const getLinks = () =>
     document.querySelectorAll("p > a:not(:has(*)), li > a:not(:has(*))");
+*/
