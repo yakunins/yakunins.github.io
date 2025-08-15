7d2261de7f151bb2e4c285721f65e4b5868081c2
Sergey Yakunin <sergey@yakunins.com>
2025-08-15 09:52:56 -0700

    v1.0.12

--- digital-clock/js/background.js
+++ digital-clock/js/background.js
@@ -1,1 +1,1 @@
-(()=>{"use strict";chrome.runtime.onMessage.addListener(((o,n)=>{var e,i,d;"open_extension_popup"===o&&(null===(i=null===(e=chrome.action)||void 0===e?void 0:e.openPopup)||void 0===i||i.call(e,{windowId:null===(d=n.tab)||void 0===d?void 0:d.windowId}))}))})();
+(()=>{"use strict";chrome.runtime.onMessage.addListener((o,n)=>{var e,i,d;"open_extension_popup"===o&&(null===(i=null===(e=chrome.action)||void 0===e?void 0:e.openPopup)||void 0===i||i.call(e,{windowId:null===(d=n.tab)||void 0===d?void 0:d.windowId}))})})();
